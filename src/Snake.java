6e42517827e80f9f4c5fbb54c2b4a1a02fde83f4
Robin Söderholm <robin.soderholm87@gmail.com>
2016-11-04 11:06:41 +0100

    First commit

--- src/Snake.java
+++ src/Snake.java
@@ -1,7 +1,9 @@
-package PACKAGE_NAME;
-
 /**
  * Created by robin on 2016-11-04.
  */
 public class Snake {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+
 }
